e8edf980ef593b6d71a61fd121a9f4e9c4d6af09
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2021-11-03 14:02:08 +0100

    change print

--- main/views.py
+++ main/views.py
@@ -74,7 +74,7 @@ def receiver(request, token):
         body = json.loads(request.body)
         print(body.keys())
         print(body['data'].keys())
-        print(body['data']['metrics'].keys())
+        print(body['data']['metrics'][0])
         return HttpResponse('In receiver: no user')
 #    try:
 #        oluser = OverlandUser.objects.get(endpoint_token=token)

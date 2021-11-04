0efb185a62378b850663c75c43787e912a89afe1
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2021-11-04 10:18:47 +0100

    bump file size limit up

--- oh_template/settings.py
+++ oh_template/settings.py
@@ -15,6 +15,8 @@ import os
 import django_heroku
 
 
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
